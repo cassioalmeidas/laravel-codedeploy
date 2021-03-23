fb2ee70f219acdaedc5a0e60804aa1b42fbfc28d
johnelauria <john@fastfwd.com>
2021-03-23 23:56:18 +0800

    Add after_install step to copy default.env

---
+++ codedeploy/after_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+cd /var/www/html
+chmod -R 777 storage
+
+# This needs to be created at buildtime via codebuild! And key:generate will no longer be necessary
+cp .env.example .env
+php artisan key:generate
