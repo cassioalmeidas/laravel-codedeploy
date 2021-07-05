c1d605cd8157407d1cd637534b086f5ebf785d6c
johnelauria <john@fastfwd.com>
2021-07-05 11:56:53 +0800

    Group all urls under /store path

--- www/routes/web.php
+++ www/routes/web.php
@@ -13,14 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::prefix('store')->name('store.')->group(function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/phpinfo', function () {
-    return view('phpinfo');
-})->name('phpinfo');
+    Route::get('/phpinfo', function () {
+        return view('phpinfo');
+    })->name('phpinfo');
 
-Route::get('/sample-broken-page', function() {
-    return "this is broken, will test rollback";
+    Route::get('/sample-broken-page', function() {
+        return "this is broken, will test rollback";
+    });
 });
